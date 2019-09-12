30dc485b289803a980b19cacecc2db82f5306565
unknown <belskiyigor29@gmail.com>
2019-09-12 18:17:06 +0300

    Update

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hi there!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
